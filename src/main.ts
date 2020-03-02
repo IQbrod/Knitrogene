9e45eedf4925db5c5386a8acd6e4f29f6884b9c4
IQbrod <iqbrod@outlook.fr>
2020-03-02 14:05:10 +0100

    Define router in main

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,8 @@ app.use(morgan(':date :method :status :url :response-time ms', { stream: logger.
 app.use(bodyparser.urlencoded({extended: false}));
 
 // Define routes used by the server
-const default_route = require('./routes/default.ts');
+const router = express.Router();
+const default_route = require('./routes/default.ts')(router);
 app.use(default_route);
 
 // Start server
--- src/routes/default.ts
+++ src/routes/default.ts
@@ -1,7 +1,6 @@
-const express = require('express');
-const router = express.Router();
 const service = require('../services/default_service.ts')
 
-router.get('/app/default', (req,res) => service.getDefault(req,res));
-
-module.exports = router;
+module.exports = (router) => {
+    router.get('/app/default', (req,res) => service.getDefault(req,res));
+    return router;
+};
